dbe4cc1ffc37f3780521342ccafc997b77b7682b
CopperUI <TitaniumKernel@gmail.com>
2026-09-25 21:15:52 +0300

    kernel.cpp

--- cpp_src/kernel.cpp
+++ cpp_src/kernel.cpp
@@ -7,7 +7,7 @@ extern "C" {
             return 0;
         } 
         else if (std::strcmp(command, "sysinfo") == 0) {
-            std::strncpy(output_buffer, "TitaniumOS v0.03\nKernel: Virtual C++ Kernel v0.03\nHost: Linux\nStatus: Operational", max_len);
+            std::strncpy(output_buffer, "TitaniumOS v0.04\nKernel: Virtual C++ Kernel v0.04\nHost: Linux\nStatus: Operational", max_len);
             return 0;
         }
         else if (std::strcmp(command, "clear") == 0) {
